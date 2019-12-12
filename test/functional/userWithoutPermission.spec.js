dcd8070a1e06ee61a1bea4d6443e8652f3bf6f80
leandro saraiva <lsaraiva@cadastra.com>
2019-12-12 18:00:06 -0300

    teste esqueci minha senha

--- test/functional/userWithoutPermission.spec.js
+++ test/functional/userWithoutPermission.spec.js
@@ -15,28 +15,28 @@ test('Usuário sem a permissao de "create_user", não pode cadastrar um novo usu
   assert,
   client
 }) => {
-  // const loginUser = {
-  //   email: 'email@cadastra.com',
-  //   password: '1234'
-  // }
-  // await Factory.model('App/Models/User').create(loginUser)
-  // const session = await client
-  //   .post('/session')
-  //   .send(loginUser)
-  //   .end()
-  // const { token } = session.body.user
-  // const response = await client
-  //   .post('/users')
-  //   .send({
-  //     name: 'novo usuario',
-  //     email: 'newuser@cadastra.com',
-  //     password: '123',
-  //     password_confirmation: '123'
-  //   })
-  //   .header('Authorization', `Bearer ${token}`)
-  //   .end()
-  // response.assertStatus(403)
-  // assert.equal(response.body.error.name, 'ForbiddenException')
+  const loginUser = {
+    email: 'email@cadastra.com',
+    password: '1234'
+  }
+  await Factory.model('App/Models/User').create(loginUser)
+  const session = await client
+    .post('/session')
+    .send(loginUser)
+    .end()
+  const { token } = session.body.user
+  const response = await client
+    .post('/users')
+    .send({
+      name: 'novo usuario',
+      email: 'newuser@cadastra.com',
+      password: '123',
+      password_confirmation: '123'
+    })
+    .header('Authorization', `Bearer ${token}`)
+    .end()
+  response.assertStatus(403)
+  assert.equal(response.body.error.name, 'ForbiddenException')
 })
 
 // test('Usuário sem a permissao de "read_user", listar todos os usuarios', async ({
